5afeece67e3968a6b7dbdf7571751926a23c7eb0
Jan Thomä <janthomae@janthomae.de>
2023-07-25 21:22:04 +0200

    chore: upgrade gdunit

--- addons/gdUnit4/src/mono/GdUnit3MonoAPI.gd
+++ addons/gdUnit4/src/mono/GdUnit3MonoAPI.gd
@@ -29,10 +29,9 @@ static func parse_test_suite(source_path :String) -> Node:
 	return instance().ParseTestSuite(source_path)
 
 static func create_executor(listener :Node) -> Node:
-	return null
-	#if not GdUnitTools.is_mono_supported():
-#		return null
-#	return instance().Executor(listener)
+	if not GdUnitTools.is_mono_supported():
+		return null
+	return instance().Executor(listener)
 
 static func is_csharp_file(resource_path :String) -> bool:
 	var ext := resource_path.get_extension()
